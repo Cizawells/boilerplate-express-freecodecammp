da27d912459a0cdf57b55f9e29168cfcc103466d
ciza Wells <40054711+Cizawells@users.noreply.github.com>
2022-09-25 13:56:08 +0000

    completed some chanllenges

--- myApp.js
+++ myApp.js
@@ -1,9 +1,26 @@
 let express = require('express');
 let app = express();
 
+app.use("/public", express.static(`${__dirname}/public`));
 
+app.use( (req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+})
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/views/index.html`);
+})
 
-
+app.get("/json", (req, res) => {
+  const mySecret = process.env['MESSAGE_STYLE'];
+let response = "Hello json";
+  if(mySecret === "uppercase") {
+    response = "Hello json".toUpperCase();
+  }else {
+       response = "Hello json";
+  }
+res.json({message: response});
+})
 
 
 
